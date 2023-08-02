bdaf1d6c1aad5a27cef7d84833184f82365716a1
Paulo Wells <paulowells@gmail.com>
2023-08-02 16:42:13 -0300

    changed clean input text button to type button

--- app/components/TextInput.tsx
+++ app/components/TextInput.tsx
@@ -20,9 +20,27 @@ const TextInput: FC<TextInputProps> = ({ className, placeHolder, label }) => {
 
     return (
         <div className={className ? `text-input ${className}` : `text-input`}>
-            <input id="term-input" type="text" name="term" placeholder={placeHolder} ref={inputRef} />
-            <button name="search button" aria-label="search button" className="input-icon input-icon__left"><Search /></button>
-            <button className="input-icon input-icon__right" onClick={resetInputText}><Close /></button>
+            <input
+                id="term-input"
+                type="text"
+                name="term"
+                placeholder={placeHolder}
+                ref={inputRef}
+            />
+            <button
+                name="search button"
+                aria-label="search button"
+                className="input-icon input-icon__left"
+                type="button"
+                >
+                <Search />
+            </button>
+            <button
+                className="input-icon input-icon__right"
+                type="button"
+                onClick={resetInputText}>
+                <Close />
+            </button>
             <label htmlFor="term-input">{label}</label>
         </div>
     );
