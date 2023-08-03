ab90eef0a8cfd15d75e18d700ed7cf53001f3807
Paulo Wells <paulowells@gmail.com>
2023-08-03 14:54:19 -0300

    added css

--- app/pages/HomePage.tsx
+++ app/pages/HomePage.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import CategoriesList from "~/components/CategoriesList";
 import Search from "~/components/SearchBar";
+import ExternalLink from "~/icons/external/ExternalLink";
 import Logo from "~/icons/logo/Logo";
 
 const HomePage: FC = () => {
@@ -12,7 +13,9 @@ const HomePage: FC = () => {
             </div>
             <h1 className="homepage__heading">
                 <span className="homepage__heading--main">Find free public APIs for your project</span>
-                <span className="homepage__heading--sub">A frontend wrapper for the <a href="https://github.com/public-apis/public-apis">public apis</a> project.</span>
+                <span className="homepage__heading--sub">
+                    A frontend wrapper for the <a href="https://github.com/public-apis/public-apis">public apis <ExternalLink /></a> project.
+                </span>
             </h1>
 
             <Search />
