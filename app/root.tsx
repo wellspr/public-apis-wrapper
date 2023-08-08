7bb3e726449813d9603266d0d5cfbb2bf3d86368
Paulo Wells <paulowells@gmail.com>
2023-08-08 14:55:04 -0300

    changed logo and favicon

--- app/root.tsx
+++ app/root.tsx
@@ -23,7 +23,11 @@ export const links = () => {
         { rel: "icon", href: "/favicon.svg", type: "image/svg+xml" },
         { rel: "manifest", href: "/site.webmanifest" },
         { rel: "preload", as: "style", href: styles },
-        { rel: "stylesheet", href: styles }
+        { rel: "stylesheet", href: styles },
+        { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" },
+        { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" },
+        { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png" },
+        { rel: "mask-icon", href: "/safari-pinned-tab.svg", color: "#5bbad5" },
     ];
 };
 
@@ -42,6 +46,8 @@ export default function App() {
             <head>
                 <meta charSet="utf-8" />
                 <meta name="viewport" content="width=device-width,initial-scale=1" />
+                <meta name="msapplication-TileColor" content="#da532c" />
+                <meta name="theme-color" content="#ffffff" />
                 <Meta />
                 <Links />
             </head>
